cdf842c4871e315c53424a7ff0fc2f6dc4437463
David Matejka <matej21@matej21.cz>
2017-09-28 16:05:56 +0200

    presenter mapper: improve classToPresenterName

--- src/org/posobota/nette/PresenterMapper.java
+++ src/org/posobota/nette/PresenterMapper.java
@@ -2,21 +2,28 @@ package org.posobota.nette;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class PresenterMapper
 {
+    private static String prefix = "App\\";
+    private static String modulePart = "*Module\\";
+    private static String presenterPart = "Presenters\\*Presenter";
 
     @Nullable
     public static String classToPresenterName(String className)
     {
-        switch (className) {
-            case "\\App\\Presenters\\HomepagePresenter":
-                return "Homepage";
-            case "\\App\\BlogModule\\Presenters\\ArticleDetailPresenter":
-                return "Blog:ArticleDetail";
-            case "\\App\\BlogModule\\Presenters\\ArticleListPresenter":
-                return "Blog:ArticleList";
+        String modulePartRegex = modulePart.replace("\\", "\\\\").replace("*", "(\\w+)");
+        Pattern pattern = Pattern.compile("^\\\\?" + prefix.replace("\\", "\\\\")
+                + "((?:" + modulePartRegex + ")*)"
+                + presenterPart.replace("\\", "\\\\").replace("*", "(\\w+)") + "\\z");
+        Matcher matcher = pattern.matcher(className);
+        if (!matcher.matches()) {
+            return null;
         }
-        return null;
+        return Pattern.compile(modulePartRegex).matcher(matcher.group(1)).replaceAll("$1:") + matcher.group(3);
+
     }
 }
