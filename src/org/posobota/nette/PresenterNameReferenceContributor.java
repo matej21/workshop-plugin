1bb54683831c9b4e99dca89b2b6dda306327dd07
David Matejka <matej21@matej21.cz>
2017-09-28 17:24:04 +0200

    register reference contributor

---
+++ src/org/posobota/nette/PresenterNameReferenceContributor.java
@@ -0,0 +1,16 @@
+package org.posobota.nette;
+
+import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+public class PresenterNameReferenceContributor extends PsiReferenceContributor
+{
+
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar psiReferenceRegistrar)
+    {
+
+    }
+
+}
