526153c3551ec0e6aa3106748b0cd3a395124425
David Matejka <matej21@matej21.cz>
2017-09-28 15:21:02 +0200

    completion example

--- src/org/posobota/nette/PresenterNameCompletionProvider.java
+++ src/org/posobota/nette/PresenterNameCompletionProvider.java
@@ -3,8 +3,12 @@ package org.posobota.nette;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
+import com.jetbrains.php.PhpIcons;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.NotNull;
 
 public class PresenterNameCompletionProvider extends CompletionProvider<CompletionParameters>
@@ -14,6 +18,7 @@ public class PresenterNameCompletionProvider extends CompletionProvider<Completi
     protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet)
     {
         PsiElement el = completionParameters.getPosition();
+        completionResultSet.addElement(LookupElementBuilder.create("Blog:ArticleDetail:").withIcon(PhpIcons.CLASS));
     }
 
 }
