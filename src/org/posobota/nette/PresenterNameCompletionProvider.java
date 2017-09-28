64c9e841a27c95beb9fe8f786c368597256cf6c9
David Matejka <matej21@matej21.cz>
2017-09-28 15:24:23 +0200

    completion: add presenter search

--- src/org/posobota/nette/PresenterNameCompletionProvider.java
+++ src/org/posobota/nette/PresenterNameCompletionProvider.java
@@ -3,13 +3,14 @@ package org.posobota.nette;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.ProcessingContext;
-import com.jetbrains.php.PhpIcons;
+import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 public class PresenterNameCompletionProvider extends CompletionProvider<CompletionParameters>
 {
@@ -18,7 +19,19 @@ public class PresenterNameCompletionProvider extends CompletionProvider<Completi
     protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet)
     {
         PsiElement el = completionParameters.getPosition();
-        completionResultSet.addElement(LookupElementBuilder.create("Blog:ArticleDetail:").withIcon(PhpIcons.CLASS));
+
+        PhpIndex index = PhpIndex.getInstance(el.getProject());
+        Collection<PhpClass> presenters = index.getClassesByFQN("Nette\\Application\\UI\\Presenter");
+        if (presenters.isEmpty()) {
+            return;
+        }
+        PhpClass presenter = presenters.iterator().next();
+
+        PhpClass[] foundPresenters = index.getAllClassNames(completionResultSet.getPrefixMatcher())
+                .stream()
+                .flatMap(s -> index.getClassesByName(s).stream())
+                .filter(cls -> presenter.getType().isConvertibleFrom(cls.getType(), index))
+                .toArray(PhpClass[]::new);
     }
 
 }
